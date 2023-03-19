78256dafc0d4bc9853c8de1da0f206b6adce5f2c
vinny90908 <viniciosmendes908@gmail.com>
2023-03-19 18:49:33 -0300

    url fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
 	favicon: 'img/eddy.jpg',
 
 	// Set the production url of your site here
-	url: 'docs-eddy.netlify.app',
+	url: 'docs.eddybot.ml',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/',
