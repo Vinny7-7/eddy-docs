254ca8ab164bf6e1b6752c173940e567a57116ed
vinny90908 <viniciosmendes908@gmail.com>
2023-03-19 18:59:59 -0300

    url fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
 	favicon: 'img/eddy.jpg',
 
 	// Set the production url of your site here
-	url: 'docs.eddybot.ml',
+	url: 'https://docs.eddybot.ml',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/',
