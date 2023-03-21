5854d29b175637d5b4ee8595e0188c0e8c6b39d8
vinny90908 <viniciosmendes908@gmail.com>
2023-03-20 22:16:28 -0300

    Inicio alterado

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,13 +41,13 @@ const config = {
 					sidebarPath: require.resolve('./sidebars.js'),
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+					//	editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
 				},
 				blog: {
 					showReadingTime: true,
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+					//editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
 				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
@@ -109,6 +109,16 @@ const config = {
 						{
 							label: 'Twitter',
 							href: 'https://twitter.com/Eddybot_',
+              },
+            ],
+          },
+				{
+					title: 'Outros',
+					items: [
+
+						{
+							label: 'Me adicione',
+							href: 'https://discord.com/oauth2/authorize?client_id=942232686024593428&permissions=322702&scope=bot%20applications.commands',
               },
             ],
           },
