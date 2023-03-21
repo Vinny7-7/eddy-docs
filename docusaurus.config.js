752a4326a63680e2a1fd3cf1bb9659b85cec43a8
vinny90908 <viniciosmendes908@gmail.com>
2023-03-20 22:26:51 -0300

    inicio alterado|rota

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'facebook', // Usually your GitHub org/user name.
-	projectName: 'docusaurus', // Usually your repo name.
+	organizationName: 'Vinny7-7', // Usually your GitHub org/user name.
+	projectName: 'docs-eddy', // Usually your repo name.
 
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
